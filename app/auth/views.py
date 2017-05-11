45094d61d1910705bcdc637135e8fdef84f7015e
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2017-05-11 19:00:52 +0200

    add messages

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ def login():
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invaild username or password.')
+        flash('Invalid username or password.', 'warning')
     return render_template('auth/login.html', form=form)
 
 
